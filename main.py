b99722a77ccdd0170abd232e49c8f4a3c3ce3d60
Jeff <mx.jeferson.12@gmail.com>
2022-10-07 12:03:33 -0300

    add response status

--- main.py
+++ main.py
@@ -1,8 +1,34 @@
 import os
+import re
+import time
+from functools import wraps
+from xmlrpc.client import ResponseError
 from scrapy.crawler import CrawlerProcess
-
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
+
+# Programa rodou em 64 seconds
+
+def execution_time(func):
+    """
+    monite the function execution time
+    """
+    @wraps(func)
+    def timeit_wrapper(*args, **kwargs):
+        start_time = time.perf_counter()
+        result = func(*args, **kwargs)
+        end_time = time.perf_counter()
+        total_time = end_time - start_time
+        print(f'Programa rodou em {total_time:.0f} seconds')
+        return result
+    return timeit_wrapper
+
+
+def strip_ponctuation(text):
+    """
+    removes the ponctiation of text and special caracters
+    """
+    return re.sub(u'[^a-zA-Z0-9áéíóúÁÉÍÓÚâêîôÂÊÎÔãõÃÕçÇ: ]', '_', text)
 
 
 def get_base_domain(url:str):
@@ -27,31 +53,32 @@ def save_to_folder(content, filename:str):
         html_file.write(content)
     
 
-
 class Infolink(CrawlSpider):
     name = 'infolink'
     
     ignore=['#','/search/','/busca/','q=']
-    start_urls = ['https://www.morpheusmedspa.com' ,'https://www.destinationaesthetics.com', 'https://www.dermacaresandiego.com', 'https://www.williamsfacialsurgery.com', 'https://www.williamsfacialsurgery.com/our-doctors/']
+    start_urls = ['https://www.morpheusmedspa.com', 'https://www.destinationaesthetics.com', 'https://www.dermacaresandiego.com', 'https://www.williamsfacialsurgery.com', 'https://www.williamsfacialsurgery.com/our-doctors/']
     allowed_domains = [get_base_domain(url) for url in start_urls]
     rules = [ Rule (LinkExtractor(deny=(ignore)), callback="parse_link",  follow=True),]
 
     def parse_link(self, response):
 
+        if response.status != 200 or response.body == "": return
+
         origin = get_base_domain(response.url)
         title = response.css('title::text').get()
-        filename = f"{origin.replace('.','_')}_{title.replace(' ','_').replace('.', '_').replace('|','_').replace('/','_').replace('?', '_')}"
-        save_to_folder(response.body, filename)
+        # filename = f"{strip_ponctuation(origin)}_{strip_ponctuation(title)}"
+        # save_to_folder(response.body, filename)
 
         yield {
             'origin': origin,
             'link': response.url, 
             'title': title,
-            'content': f"site_body/{filename}"
+            'content': str(response.body) #f"site_body/{filename}"
         }
 
 settings = {
-    'DEPTH_LIMIT': 2,
+    # 'DEPTH_LIMIT': 2,
     'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)', 
     # descomentar aqui se usar o recurso de insert a cada x dados
     'CONCURRENT_REQUESTS': 100, # default to 16
@@ -60,21 +87,22 @@ settings = {
 
     'LOG_LEVEL': 'INFO',
     'COOKIES_ENABLED': False,
-    # 'SCHEDULER_PRIORITY_QUEUE': 'scrapy.pqueues.DownloaderAwarePriorityQueue',
-    # 'REACTOR_THREADPOOL_MAXSIZE': 20,
-    # 'RETRY_ENABLED': False,
+    'SCHEDULER_PRIORITY_QUEUE': 'scrapy.pqueues.DownloaderAwarePriorityQueue',
+    'REACTOR_THREADPOOL_MAXSIZE': 20,
+    'RETRY_ENABLED': False,
     'DOWNLOAD_TIMEOUT': 15,
-    # 'REDIRECT_ENABLED': False,
+    'REDIRECT_ENABLED': False,
     'AJAXCRAWL_ENABLED': True,
-    # 'DEPTH_PRIORITY': 1,
-    # 'SCHEDULER_DISK_QUEUE': 'scrapy.squeues.PickleFifoDiskQueue',
-    # 'SCHEDULER_MEMORY_QUEUE': 'scrapy.squeues.FifoMemoryQueue',
+    'DEPTH_PRIORITY': 1,
+    'SCHEDULER_DISK_QUEUE': 'scrapy.squeues.PickleFifoDiskQueue',
+    'SCHEDULER_MEMORY_QUEUE': 'scrapy.squeues.FifoMemoryQueue',
     # 'LOG_FILE': 'crawler.log' # logging file
     'FEEDS': {
         'data.csv': {'format': 'csv'}
     }
 }
 
+@execution_time
 def main():
     process = CrawlerProcess(settings)
     process.crawl(Infolink)
