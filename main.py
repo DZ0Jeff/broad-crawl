b8fb9460fe19106290592a2cda4fbe810ebd93f4
Jeff <mx.jeferson.12@gmail.com>
2022-10-14 18:56:44 -0300

    add xlsx

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import os
 import re
 import time
-from functools import wraps
-from xmlrpc.client import ResponseError
+import multiprocessing
+
+from functools import wraps, partial
 from scrapy.crawler import CrawlerProcess
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
 
 # Programa rodou em 64 seconds
+SAVE_DIRECTORY = 'data'
+
 
 def execution_time(func):
     """
@@ -42,23 +45,64 @@ def get_base_domain(url:str):
     return url
 
 
-def save_to_folder(content, filename:str):
+def save_to_folder(content, filename:str, parts_dir:str='site_body'):
     """
     save the html content inside of folder with site name
     """
-    if not os.path.exists('site_body'):
-        os.makedirs('site_body')
+    if not os.path.exists(f'{SAVE_DIRECTORY}/{parts_dir}'):
+        os.makedirs(f'{SAVE_DIRECTORY}/{parts_dir}')
 
-    with open(f"site_body/{filename}.html", 'wb') as html_file:
+    with open(f"{SAVE_DIRECTORY}/{parts_dir}/{filename}.html", 'wb') as html_file:
         html_file.write(content)
     
 
-class Infolink(CrawlSpider):
-    name = 'infolink'
+def spider_worker(spider, target_url:str):
+    urls = [target_url]
+    settings = {
+        'DEPTH_LIMIT': 2,
+        'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)', 
+        # descomentar aqui se usar o recurso de insert a cada x dados
+        'CONCURRENT_REQUESTS': 100, # default to 16
+        'CONCURRENT_REQUESTS_PER_DOMAIN': 16, # default to 8
+        # 'CONCURRENT_REQUESTS_PER_IP': 0, # default to 0
+
+        'LOG_LEVEL': 'INFO',
+        'COOKIES_ENABLED': False,
+        'SCHEDULER_PRIORITY_QUEUE': 'scrapy.pqueues.DownloaderAwarePriorityQueue',
+        'REACTOR_THREADPOOL_MAXSIZE': 20,
+        'RETRY_ENABLED': False,
+        'DOWNLOAD_TIMEOUT': 15,
+        'REDIRECT_ENABLED': False,
+        'AJAXCRAWL_ENABLED': True,
+        'DEPTH_PRIORITY': 1,
+        'SCHEDULER_DISK_QUEUE': 'scrapy.squeues.PickleFifoDiskQueue',
+        'SCHEDULER_MEMORY_QUEUE': 'scrapy.squeues.FifoMemoryQueue',
+        # 'LOG_FILE': 'crawler.log' # logging file
+        'FEED_EXPORTERS': {
+            'xlsx': 'scrapy_xlsx.XlsxItemExporter',
+        },
+        'FEEDS': {
+            f'{SAVE_DIRECTORY}/data.xlsx': {'format': 'xlsx'}
+        }
+    }
+
+    process = CrawlerProcess(settings)
+    process.crawl(spider, start_urls=urls, allowed_domains = [get_base_domain(url) for url in urls])
+    process.start()
+
+    import sys
+    del sys.modules['twisted.internet.reactor']
+    from twisted.internet import reactor
+    from twisted.internet import default
+    default.install()
+
+
+class BroadCrawler(CrawlSpider):
+    name = 'broad_crawler'
     
+    # start_urls = ['https://www.morpheusmedspa.com', 'https://www.destinationaesthetics.com', 'https://www.dermacaresandiego.com', 'https://www.williamsfacialsurgery.com', 'https://www.williamsfacialsurgery.com/our-doctors/']
+    # allowed_domains = [get_base_domain(url) for url in start_urls]
     ignore=['#','/search/','/busca/','q=']
-    start_urls = ['https://www.morpheusmedspa.com', 'https://www.destinationaesthetics.com', 'https://www.dermacaresandiego.com', 'https://www.williamsfacialsurgery.com', 'https://www.williamsfacialsurgery.com/our-doctors/']
-    allowed_domains = [get_base_domain(url) for url in start_urls]
     rules = [ Rule (LinkExtractor(deny=(ignore)), callback="parse_link",  follow=True),]
 
     def parse_link(self, response):
@@ -74,39 +118,19 @@ class Infolink(CrawlSpider):
             'origin': origin,
             'link': response.url, 
             'title': title,
-            'content': str(response.body) #f"site_body/{filename}"
+            'content': str(response.body) #f"site_body/{filename}" #
         }
 
-settings = {
-    # 'DEPTH_LIMIT': 2,
-    'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)', 
-    # descomentar aqui se usar o recurso de insert a cada x dados
-    'CONCURRENT_REQUESTS': 100, # default to 16
-    # 'CONCURRENT_REQUESTS_PER_DOMAIN': 16, # default to 8
-    # 'CONCURRENT_REQUESTS_PER_IP': 0, # default to 0
-
-    'LOG_LEVEL': 'INFO',
-    'COOKIES_ENABLED': False,
-    'SCHEDULER_PRIORITY_QUEUE': 'scrapy.pqueues.DownloaderAwarePriorityQueue',
-    'REACTOR_THREADPOOL_MAXSIZE': 20,
-    'RETRY_ENABLED': False,
-    'DOWNLOAD_TIMEOUT': 15,
-    'REDIRECT_ENABLED': False,
-    'AJAXCRAWL_ENABLED': True,
-    'DEPTH_PRIORITY': 1,
-    'SCHEDULER_DISK_QUEUE': 'scrapy.squeues.PickleFifoDiskQueue',
-    'SCHEDULER_MEMORY_QUEUE': 'scrapy.squeues.FifoMemoryQueue',
-    # 'LOG_FILE': 'crawler.log' # logging file
-    'FEEDS': {
-        'data.csv': {'format': 'csv'}
-    }
-}
 
 @execution_time
 def main():
-    process = CrawlerProcess(settings)
-    process.crawl(Infolink)
-    process.start()
+    urls = ['https://www.morpheusmedspa.com', 'https://www.destinationaesthetics.com', 'https://www.dermacaresandiego.com', 'https://www.williamsfacialsurgery.com', 'https://www.williamsfacialsurgery.com/our-doctors/']
+
+    if not os.path.exists(SAVE_DIRECTORY):
+        os.mkdir(SAVE_DIRECTORY)
+
+    with multiprocessing.Pool(5) as pool: #maxtasksperchild=1
+        pool.map(partial(spider_worker, BroadCrawler), urls)
 
 
 if __name__ == "__main__":
