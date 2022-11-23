d8ec10964bed6c0c45f5f3818847749392292c18
DZ0Jeff <mx.jeferson.12@gmail.com>
2022-11-23 16:53:27 -0300

    add setup.sh

--- transform.py
+++ transform.py
@@ -3,35 +3,39 @@ from scrapper_boilerplate import dataToCSV
 import urllib.parse
 
 
-def base_url(url, with_path=False):
-    parsed = urllib.parse.urlparse(url)
-    path   = '/'.join(parsed.path.split('/')[:-1]) if with_path else ''
-    parsed = parsed._replace(path=path)
-    parsed = parsed._replace(params='')
-    parsed = parsed._replace(query='')
-    parsed = parsed._replace(fragment='')
-    return parsed.geturl()
+# def base_url(url, with_path=False):
+#     parsed = urllib.parse.urlparse(url)
+#     path   = '/'.join(parsed.path.split('/')[:-1]) if with_path else ''
+#     parsed = parsed._replace(path=path)
+#     parsed = parsed._replace(params='')
+#     parsed = parsed._replace(query='')
+#     parsed = parsed._replace(fragment='')
+#     return parsed.geturl()
 
 
-df = pd.read_csv('data.csv')        
+# df = pd.read_csv('data.csv')        
     
-for i in range(len(df)):
-    print(f"{i}/{len(df)}")
-    if i < 51772: continue
-    data = dict()
+# for i in range(len(df)):
+#     print(f"{i}/{len(df)}")
+#     if i < 51772: continue
+#     data = dict()
 
-    origin = df['origin'].iloc[i]
-    link = df['link'].iloc[i]
-    title = df['title'].iloc[i]
-    content = df['content'].iloc[i]
+#     origin = df['origin'].iloc[i]
+#     link = df['link'].iloc[i]
+#     title = df['title'].iloc[i]
+#     content = df['content'].iloc[i]
 
-    try:
-        data['origin'] = base_url(link)
-        data['link'] = link
-        data['title'] = title
-        data['content'] = content
+#     try:
+#         data['origin'] = base_url(link)
+#         data['link'] = link
+#         data['title'] = title
+#         data['content'] = content
 
-        dataToCSV(data, 'filtered-origin.csv')
+#         dataToCSV(data, 'filtered-origin.csv')
 
-    except Exception:
-        continue
+#     except Exception:
+#         continue
+
+df = pd.read_excel('assets/pendentes.xlsx')
+
+print(len(df) / 2)
